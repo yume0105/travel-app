f7e7aa820f45ac3a380231fca8b1cd5e6a03bbc3
升屋 結女 <yume.masuya@gmail.com>
2025-08-17 14:56:58 +0900

    Add .gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env.GITHUB_TOKEN); // ここでトークンが参照できる
+};
