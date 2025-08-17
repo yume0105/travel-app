c5f2db89584de94309d889a50f4e1beab4cfe3d2
升屋 結女 <yume.masuya@gmail.com>
2025-08-17 20:36:32 +0900

    edit vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-export default ({ mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   console.log(env.GITHUB_TOKEN); // ここでトークンが参照できる
-};
+  return {
+    plugins: [react()],
+    // 必要なら他の設定もここに追加
+  }
+});
